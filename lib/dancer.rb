122ea90108cb002e026354a8f39c4f87887218cc
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-10 00:28:46 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,11 @@
 # require_relative './class_methods_module.rb'
 # require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer
-  
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  # include Dance
   
   attr_accessor :name
   
@@ -11,6 +13,6 @@ class Dancer
     @name = name
   end
   
-  extend MetaDancing
+  # extend MetaDancing
   
 end
